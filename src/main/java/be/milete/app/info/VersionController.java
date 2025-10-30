4870c43a9bc178dcca40d1536977806247a21a1e
DockxGi <ginodockx@gmail.com>
2025-10-30 21:52:13 +0100

    add POST go/info/version/equals endpoint

--- src/main/java/be/milete/app/info/VersionController.java
+++ src/main/java/be/milete/app/info/VersionController.java
@@ -1,19 +1,30 @@
 package be.milete.app.info;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import static org.springframework.util.Assert.isTrue;
 
 @RestController
 @RequestMapping("info/version")
 public class VersionController {
 
+    private static final String VERSION = "0.0.1";
+
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public String getVersion(){
-        return "0.0.1";
+        return VERSION;
     }
 
+    @PostMapping(value = "equals")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isVersion(@RequestBody String otherVersion){
+        isTrue(isNotBlank(otherVersion), "version must not be blank");
+        isTrue(otherVersion.length() < 16, "version must be max 16 characters");
+        String trimmedVersion = otherVersion.trim();
+
+        return VERSION.equals(trimmedVersion);
+    }
 }

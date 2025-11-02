6495daa1826e4db60cfc3078bfef7bedbc1ff535
DockxGi <ginodockx@gmail.com>
2025-11-02 16:23:13 +0100

    add live api-test for GET info/version endpoint

---
+++ src/test/java/apitests/GetInfoVersionTest.java
@@ -0,0 +1,25 @@
+package apitests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+/**
+ * Tests tge GET /info/version endpoint. Requires a live running http-api-go server.
+ */
+class GetInfoVersionTest {
+
+    private final WebTestClient webClient = WebTestClient.bindToServer()
+            .baseUrl("http://localhost:8080/go")
+            .build();
+
+    @Test
+    void shouldReturnOkAndVersion(){
+        webClient.get()
+                .uri("/info/version")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(String.class)
+                .isEqualTo("0.0.1");
+    }
+}

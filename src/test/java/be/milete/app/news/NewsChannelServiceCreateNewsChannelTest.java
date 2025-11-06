2e18882aa45a16e6c7e06c18c92d52eaee2c5c8b
DockxGi <ginodockx@gmail.com>
2025-11-06 23:03:13 +0100

    add NewsChannelServiceCreateNewsChannelTest

---
+++ src/test/java/be/milete/app/news/NewsChannelServiceCreateNewsChannelTest.java
@@ -0,0 +1,62 @@
+package be.milete.app.news;
+
+import be.milete.app.exception.NotUniqueValueException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class NewsChannelServiceCreateNewsChannelTest {
+
+    @InjectMocks
+    private NewsChannelService service;
+
+    @Mock
+    private NewsChannelRepository repository;
+
+    @Captor
+    private ArgumentCaptor<NewsChannel> newsChannelCaptor;
+
+    @Test
+    void givenNullThrowsIllegalArgumentException(){
+        Executable doCreateNewsChannel = () -> service.createNewsChannel(null);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, doCreateNewsChannel);
+        assertEquals("request must not be null", exception.getMessage());
+        verifyNoInteractions(repository);
+    }
+
+    @Test
+    void givenNameAlreadyInUseThrowsNotUniqueValueException(){
+        NewsChannelRequest request = new NewsChannelRequest("theName", "theDescription");
+        when(repository.existsByName("theName")).thenReturn(true);
+
+        Executable doCreateNewsChannel = () -> service.createNewsChannel(request);
+
+        NotUniqueValueException exception = assertThrows(NotUniqueValueException.class, doCreateNewsChannel);
+        assertEquals("name must be unique and is already in use", exception.getMessage());
+        verify(repository, times(0)).save(any());
+    }
+
+    @Test
+    void givenRequestSavesNewsChannel(){
+        NewsChannelRequest request = new NewsChannelRequest("theName", "theDescription");
+
+        service.createNewsChannel(request);
+
+        verify(repository, times(1)).save(newsChannelCaptor.capture());
+        NewsChannel newsChannel = newsChannelCaptor.getValue();
+        assertEquals("theName", newsChannel.getName());
+        assertEquals("theDescription", newsChannel.getDescription());
+    }
+}

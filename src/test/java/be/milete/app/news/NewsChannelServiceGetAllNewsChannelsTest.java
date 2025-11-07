d57c869485ba4ee7c11a06dd1a419aee5929a2c1
DockxGi <ginodockx@gmail.com>
2025-11-07 10:00:53 +0100

    add NewsChannelServiceGetAllNewsChannelsTest

---
+++ src/test/java/be/milete/app/news/NewsChannelServiceGetAllNewsChannelsTest.java
@@ -0,0 +1,55 @@
+package be.milete.app.news;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class NewsChannelServiceGetAllNewsChannelsTest {
+
+    @InjectMocks
+    private NewsChannelService service;
+
+    @Mock
+    private NewsChannelRepository repository;
+
+    @Test
+    void returnsListOfNewsChannelResponses(){
+        NewsChannel newsChannel1 = mockNewsChannel(1,"name1");
+        NewsChannel newsChannel2 = mockNewsChannel(2,"name2");
+        when(repository.findAll()).thenReturn(asList(newsChannel1, newsChannel2));
+
+        List<NewsChannelResponse> allNewsChannels = service.getAllNewsChannels();
+
+        assertEquals(2, allNewsChannels.size());
+        assertTrue(containsNewsChannel(1, "name1", allNewsChannels));
+        assertTrue(containsNewsChannel(2, "name2", allNewsChannels));
+    }
+
+    private NewsChannel mockNewsChannel(int id, String name){
+        NewsChannel mock = mock(NewsChannel.class);
+        when(mock.getId()).thenReturn(id);
+        when(mock.getName()).thenReturn(name);
+
+        return mock;
+    }
+
+    private boolean containsNewsChannel(int id, String name, List<NewsChannelResponse> newsChannels){
+        return newsChannels.stream()
+                .anyMatch(r -> r.id() == id && name.equals(r.name()));
+    }
+
+}

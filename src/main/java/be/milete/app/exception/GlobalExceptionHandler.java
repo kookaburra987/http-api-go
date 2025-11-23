ed7b777d8c34205220e0f295e639e346784525f5
DockxGi <ginodockx@gmail.com>
2025-11-23 13:51:38 +0100

    add logging to GlobalExceptionHandler

--- src/main/java/be/milete/app/exception/GlobalExceptionHandler.java
+++ src/main/java/be/milete/app/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package be.milete.app.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,34 +15,36 @@ import static org.springframework.http.HttpStatus.*;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     /**
      * Handler for exceptions that are not handled by other methods.
      */
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGlobalException(Exception e){
         ErrorResponse responseBody = new ErrorResponse("Unexpected error");
-        //todo: log the exception
+        logger.error("Exception occurred", e);
         return new ResponseEntity<>(responseBody, INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(){
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e){
         ErrorResponse responseBody = new ErrorResponse("HTTP message not readable");
-        //todo: log the exception
+        logger.warn("HTTP message not readable", e);
         return new ResponseEntity<>(responseBody, BAD_REQUEST);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException e){
         ErrorResponse responseBody = new ErrorResponse(e.getMessage());
-        //todo: log the exception
+        logger.warn("Resource not found", e);
         return new ResponseEntity<>(responseBody, NOT_FOUND);
     }
 
     @ExceptionHandler(NotUniqueValueException.class)
     public ResponseEntity<ErrorResponse> handleNotUniqueValueException(NotUniqueValueException e){
         ErrorResponse responseBody = new ErrorResponse(e.getMessage());
-        //todo: log the exception
+        logger.warn("Value not unique", e);
         return new ResponseEntity<>(responseBody, CONFLICT);
     }
 }
